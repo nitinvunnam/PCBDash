c8e3510570080628e02347908c8460cdc09bd6f1
Aditya Pulipaka <adipu@wireless-10-148-198-241.public.utexas.edu>
2025-04-15 22:12:55 -0500

    initial

---
+++ Debug/SmallFont.d
@@ -0,0 +1,7 @@
+# FIXED
+
+SmallFont.o: ../SmallFont.cpp \
+ /Users/adipu/Library/CloudStorage/OneDrive-TheUniversityofTexasatAustin/Freshman/319H/MSPM0_ValvanoWare/ECE319K_Lab9H/SmallFont.h \
+ ../../inc/ST7735.h
+/Users/adipu/Library/CloudStorage/OneDrive-TheUniversityofTexasatAustin/Freshman/319H/MSPM0_ValvanoWare/ECE319K_Lab9H/SmallFont.h:
+../../inc/ST7735.h:
